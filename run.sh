1d460e5bc8105acd032ab30d51c7a220c2bb8b70
Olof Olsson <olof.olsson@snd.gu.se>
2025-10-06 21:38:12 +0200

    fix some scripting errors

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ ! -d "research-output-aggregator" ]; then
     git clone https://github.com/snd-sweden/research-output-aggregator
 else
     echo "Directory research-output-aggregator exists, pulling latest changes..."
-    (cd research-output-aggregator && git pull)
+    cd research-output-aggregator && git pull && pip install -e . && cd ..
 fi
 
 
@@ -24,11 +24,11 @@ while IFS=$'\t' read -r -a cols || [ -n "${cols[*]}" ]; do
     if [ -f "research-output-aggregator/tests/name-lists/$org_slug.txt" ]; then
         name_txt="research-output-aggregator/tests/name-lists/$org_slug.txt"
         echo "Processing ror+txt $org_slug $org_name_en ror: $org_ror"
-        python3 ./research-output-aggregator/src/roagg/cli.py --ror "$org_ror" --name-txt "$name_txt" --output "outputs/$org_slug.csv"
+        roagg --ror "$org_ror" --name-txt "$name_txt" --output "outputs/$org_slug.csv"
 
     else
         echo "Processing ror $org_slug $org_name_en ror: $org_ror"
-        python3 ./research-output-aggregator/src/roagg/cli.py --ror "$org_ror" --output "outputs/$org_slug.csv"
+        roagg --ror "$org_ror" --output "outputs/$org_slug.csv"
     fi
 done < "$TSV"
 
