94a53688628d550b785a837a84902132a2027761
Olof Olsson <olof.olsson@snd.gu.se>
2025-10-20 13:55:17 +0200

    Add OpenAire datasets

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ ! -d "research-output-aggregator" ]; then
     git clone https://github.com/snd-sweden/research-output-aggregator
 else
     echo "Directory research-output-aggregator exists, pulling latest changes..."
-    cd research-output-aggregator && git pull && pip install -e . && cd ..
+    cd research-output-aggregator && git pull && pip install . && cd ..
 fi
 
 
@@ -16,6 +16,11 @@ TSV="$DIR/resources/organisations.tsv"
 while IFS=$'\t' read -r -a cols || [ -n "${cols[*]}" ]; do
     [[ ${#cols[@]} -eq 0 ]] && continue
 
+    #skip header line
+    if [ "${cols[0]}" == "slug" ]; then
+        continue
+    fi
+
     org_slug="${cols[0]}"
     org_name_en="${cols[1]}"
     org_name_sv="${cols[2]}"
@@ -23,13 +28,16 @@ while IFS=$'\t' read -r -a cols || [ -n "${cols[*]}" ]; do
 
     if [ -f "research-output-aggregator/tests/name-lists/$org_slug.txt" ]; then
         name_txt="research-output-aggregator/tests/name-lists/$org_slug.txt"
-        echo "Processing ror+txt $org_slug $org_name_en ror: $org_ror"
+        echo "Processing ROR+TXT for $org_slug ($org_name_en) ROR: $org_ror"
         roagg --ror "$org_ror" --name-txt "$name_txt" --output "outputs/$org_slug.csv"
 
     else
-        echo "Processing ror $org_slug $org_name_en ror: $org_ror"
+        echo "Processing ROR for $org_slug ($org_name_en) ROR: $org_ror"
         roagg --ror "$org_ror" --output "outputs/$org_slug.csv"
     fi
+    
+    echo "Sleeping for 5 minutes to avoid rate limiting..."
+    sleep 300
 done < "$TSV"
 
 echo "Add clientName and clientType to all files"
